b16cb7700ec3d60cb513333fd1d559b8a1c00957
ckdfs <2459317008@qq.com>
2024-06-06 05:19:20 +0800

    feat: optimize navigation component
    The navigation component has been optimized by removing unnecessary code and adding a close button functionality.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,21 @@
+/*
+ * @Author: ckdfs 2459317008@qq.com
+ * @Date: 2024-06-06 00:06:38
+ * @LastEditors: ckdfs 2459317008@qq.com
+ * @LastEditTime: 2024-06-06 05:16:35
+ * @FilePath: \agricultural-big-data\src\components\Navigation.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 
 import { $icon } from '../utils';
 
 
 export default function Navigation() {
+    const handleClose = () => {
+        window.close();
+    };
+
     return (
         <div className='navigation'>
             <div className='center'>
@@ -16,11 +28,11 @@ export default function Navigation() {
                 </div>
             </div>
             <div className='tool'>
-                <div className='tool-item'>
+                {/* <div className='tool-item'>
                     <img src={$icon('setting')} alt='' />
                     <span>设置</span>
-                </div>
-                <div className='tool-item'>
+                </div> */}
+                <div className='tool-item' onClick={handleClose}>
                     <img src={$icon('poweroff')} alt='' />
                     <span>退出</span>
                 </div>
