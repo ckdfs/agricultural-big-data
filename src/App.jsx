f1dd99bada3a073908253878f8c10810a6678f24
ckdfs <2459317008@qq.com>
2024-06-09 01:47:13 +0800

    feat: add Control component and update App.jsx
    The Control component is added to the App.jsx file to enhance the functionality of the Industrial section. This commit also updates the import statements in App.jsx to reflect the changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+/*
+ * @Author: ckdfs 2459317008@qq.com
+ * @Date: 2024-06-06 00:06:38
+ * @LastEditors: ckdfs 2459317008@qq.com
+ * @LastEditTime: 2024-06-09 00:57:51
+ * @FilePath: \agricultural-big-data\src\App.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 
 import Navigation from './components/Navigation';
 import Natural from './containers/Natural';
 import Farming from './containers/Farming';
 import Industrial from './containers/Industrial';
-import Advantage from './containers/Advantage';
 import Profittrend from './containers/Profittrend';
+import Control from './containers/Control';
 
 export default function App() {
     return (
@@ -17,7 +25,7 @@ export default function App() {
                 </div>
                 <div className='main-right'>
                     <Industrial />
-                    <Advantage />
+                    <Control />
                 </div>
                 <div className='main-center'>
                     <div className='main-center-panel'>
---
+++ src/containers/Control.jsx
@@ -0,0 +1,64 @@
+/*
+ * @Author: ckdfs 2459317008@qq.com
+ * @Date: 2024-06-09 00:56:24
+ * @LastEditors: ckdfs 2459317008@qq.com
+ * @LastEditTime: 2024-06-09 01:41:58
+ * @FilePath: \agricultural-big-data\src\containers\Control.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React, { useState } from 'react';
+import AnNumber from 'animated-number-react';
+import Layout from '../layouts/Box';
+
+const MarkTitle = ({ title }) => (
+    <div className='mark-title'>
+        <span className='dot' />{title}
+    </div>
+);
+
+export default function Control() {
+    // 初始化按钮状态
+    const [buttonStates, setButtonStates] = useState({
+        fan1: false,
+        fan2: false,
+        pump1: false,
+        pump2: false,
+    });
+
+    // 切换按钮状态的函数
+    const toggleButtonState = (button) => {
+        setButtonStates(prevState => ({
+            ...prevState,
+            [button]: !prevState[button],
+        }));
+    };
+
+    return (
+        <Layout title='控制按钮' minTitle='Control Button'>
+            <div className='control'>
+                <MarkTitle title='风扇' />
+                <div className='list'>
+                    <div className='list-item' onClick={() => toggleButtonState('fan1')}>
+                        <div className='list-title'>风扇1</div>
+                        <div className='list-val'>{buttonStates.fan1 ? '开' : '关'}</div>
+                    </div>
+                    <div className='list-item' onClick={() => toggleButtonState('fan2')}>
+                        <div className='list-title'>风扇2</div>
+                        <div className='list-val'>{buttonStates.fan2 ? '开' : '关'}</div>
+                    </div>
+                </div>
+                <MarkTitle title='水泵' />
+                <div className='list' style={{ padding: '0 12px' }}>
+                    <div className='list-item' onClick={() => toggleButtonState('pump1')}>
+                        <div className='list-title'>水泵1</div>
+                        <div className='list-val'>{buttonStates.pump1 ? '开' : '关'}</div>
+                    </div>
+                    <div className='list-item' onClick={() => toggleButtonState('pump2')}>
+                        <div className='list-title'>水泵2</div>
+                        <div className='list-val'>{buttonStates.pump2 ? '开' : '关'}</div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
